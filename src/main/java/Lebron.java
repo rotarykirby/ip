8d45b56253018269ef5684c411da0a9ba466f2c5
Ruixuan <e1406360@u.nus.edu>
2025-08-22 03:03:48 +0800

    Add ability to store text and display when requested
    Add ability to store text in an array and display it back when the user
    requests for it by typing "list"

--- src/main/java/Lebron.java
+++ src/main/java/Lebron.java
@@ -1,6 +1,41 @@
 import java.util.Scanner;
 
 public class Lebron {
+    public final static int SIZE = 100;
+
+    public static void handleList(String[] listArray) {
+        System.out.println("    ____________________________________________________________");
+        for (int i = 0; i < SIZE; i++) {
+            if (listArray[i] == null) {
+                break;
+            } else {
+                System.out.println(String.format("     %d. ", (i + 1)) + listArray[i]);
+            }
+        }
+        System.out.println("    ____________________________________________________________");
+    }
+
+    public static void handleInputs() {
+        // check for user input and terminate if user says "bye"
+        Scanner sc = new Scanner(System.in);
+        String[] listArray = new String[SIZE];
+        int i = 0;
+
+        String input = sc.nextLine();
+        while (!input.equals("bye")) {
+            if (input.equals("list")) {
+                handleList(listArray);
+            } else {
+                listArray[i] = input;
+                ++i;
+                System.out.println("    ____________________________________________________________\n" +
+                        "    added: " + input +
+                        "\n    ____________________________________________________________");
+            }
+            input = sc.nextLine();
+        }
+    }
+
     public static void main(String[] args) {
         String greet = "    ____________________________________________________________\n" +
                 "    Hello! I'm Lebron\n" +
@@ -13,15 +48,9 @@ public class Lebron {
         // initial greeting
         System.out.println(greet);
 
-        Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-        while (!input.equals("bye")) {
-            System.out.println("    ____________________________________________________________\n    " +
-                    input +
-                    "\n    ____________________________________________________________");
-            input = sc.nextLine();
-        }
+        handleInputs();
 
+        // goodbye message
         System.out.println("    ____________________________________________________________\n" + bye);
     }
 }
