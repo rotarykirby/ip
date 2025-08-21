ad77010946f0c6cda283ee1951ce5f4eb47dba24
Ruixuan <e1406360@u.nus.edu>
2025-08-22 03:44:44 +0800

    Add the ability to mark tasks as done/not done
    Add the ability to mark tasks as done/not done. Make class for tasks.

--- src/main/java/Lebron.java
+++ src/main/java/Lebron.java
@@ -1,38 +1,63 @@
 import java.util.Scanner;
 
 public class Lebron {
+
     public final static int SIZE = 100;
 
-    public static void handleList(String[] listArray) {
-        System.out.println("    ____________________________________________________________");
+    public static void handleList(Task[] taskArray) {
+        System.out.println("    ____________________________________________________________\n" +
+                "    Here are the tasks in your list:");
         for (int i = 0; i < SIZE; i++) {
-            if (listArray[i] == null) {
+            if (taskArray[i] == null) {
                 break;
             } else {
-                System.out.println(String.format("     %d. ", (i + 1)) + listArray[i]);
+                System.out.println(String.format("     %d. [%s] ", (i + 1), taskArray[i].getStatusIcon()) +
+                        taskArray[i].getDescription());
             }
         }
         System.out.println("    ____________________________________________________________");
     }
 
+    public static void mark(Task t, Task[] taskArray) {
+        int toMark = Integer.parseInt(t.getDescription().substring(5)) - 1;
+        taskArray[toMark].markDone();
+        System.out.println("    ____________________________________________________________\n" +
+                "    Nice! I've marked this task as done:\n" +
+                "      [X] " + taskArray[toMark].getDescription() +
+                "\n    ____________________________________________________________");
+    }
+
+    public static void unmark(Task t, Task[] taskArray) {
+        int toUnmark = Integer.parseInt(t.getDescription().substring(7)) - 1;
+        taskArray[toUnmark].unmarkDone();
+        System.out.println("    ____________________________________________________________\n" +
+                "    OK, I've marked this task as not done yet:\n" +
+                "      [ ] " + taskArray[toUnmark].getDescription() +
+                "\n    ____________________________________________________________");
+    }
+
     public static void handleInputs() {
         // check for user input and terminate if user says "bye"
         Scanner sc = new Scanner(System.in);
-        String[] listArray = new String[SIZE];
+        Task[] taskArray = new Task[SIZE];
         int i = 0;
 
-        String input = sc.nextLine();
-        while (!input.equals("bye")) {
-            if (input.equals("list")) {
-                handleList(listArray);
+        Task t = new Task(sc.nextLine());
+        while (!t.getDescription().equals("bye")) {
+            if (t.getDescription().equals("list")) {
+                handleList(taskArray);
+            } else if (t.getDescription().startsWith("mark ")) {
+                mark(t, taskArray);
+            } else if (t.getDescription().startsWith("unmark")) {
+                unmark(t, taskArray);
             } else {
-                listArray[i] = input;
+                taskArray[i] = t;
                 ++i;
                 System.out.println("    ____________________________________________________________\n" +
-                        "    added: " + input +
+                        "    added: " + t.getDescription() +
                         "\n    ____________________________________________________________");
             }
-            input = sc.nextLine();
+            t = new Task(sc.nextLine());
         }
     }
 
