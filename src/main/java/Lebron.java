ff7d60e146061865a2802af560b7e1b94f97e743
Ruixuan <e1406360@u.nus.edu>
2025-08-21 23:30:40 +0800

    Rename Duke to Lebron and add greeting
    Rename Duke to Lebron and add greeting

---
+++ src/main/java/Lebron.java
@@ -0,0 +1,19 @@
+public class Lebron {
+    public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+
+        String greet = "____________________________________________________________\n" +
+                "Hello! I'm Lebron\n" +
+                "What can I do for you?\n" +
+                "____________________________________________________________\n";
+
+        String bye = "Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________";
+
+        System.out.println(greet + bye);
+    }
+}
